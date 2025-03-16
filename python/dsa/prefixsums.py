508c1b5b04166f84759f55e5890add4c44fc83ce
dtn-prog <dtncode@gmail.com>
2025-03-16 18:26:03 +0700

    adding notes

--- python/dsa/prefixsums.py
+++ python/dsa/prefixsums.py
@@ -1,3 +1,6 @@
+# giố ng như prefixsum ta cũng có postfixsum bắ t đầ u từ cuối,
+# còn có prefixproducts và postfixproducts, product ở đây là tích, phép nhân
+
 class PrefixSums:
     def __init__(self, nums):
         self.prefixsums = []
