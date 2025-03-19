254242eccf40aae99ab65516e19388f4a38e0957
dtn-prog <dtncode@gmail.com>
2025-03-19 23:02:47 +0700

    format deno 2 spaces for tab, 125 valid palinedrome ts

--- tsDeno/125ValidPalindrome.ts
+++ tsDeno/125ValidPalindrome.ts
@@ -1,11 +1,25 @@
-const isAlphanumeric = (char: string):boolean => {
-    if(char.length !== 1) {
-        throw new Error('phai la a character')
-    }
-    const charAsciNum = char.charCodeAt(0);
-    return (
-    (charAsciNum >= 'a'.charCodeAt(0) && charAsciNum <= 'z'.charCodeAt(0))||
-    (charAsciNum >= 'A'.charCodeAt(0) && charAsciNum <= 'Z'.charCodeAt(0))||
-    (charAsciNum >= '0'.charCodeAt(0) && charAsciNum <= '9'.charCodeAt(0))
-    );
+const isAlphanumeric = (char: string): boolean => {
+  if (char.length !== 1) {
+    throw new Error("char must be a single character");
+  }
+  const charAsciNum = char.charCodeAt(0);
+  return (
+    (charAsciNum >= "a".charCodeAt(0) && charAsciNum <= "z".charCodeAt(0)) ||
+    (charAsciNum >= "A".charCodeAt(0) && charAsciNum <= "Z".charCodeAt(0)) ||
+    (charAsciNum >= "0".charCodeAt(0) && charAsciNum <= "9".charCodeAt(0))
+  );
+};
+
+function isPalindrome(s: string): boolean {
+	const size = s.length
+	for(let i=0,j=size-1; i < j; i++,j--) {
+		while(!isAlphanumeric(s[i]) && i < j)
+			i++
+		while(!isAlphanumeric(s[j]) && i < j)
+			j--
+		if (s[i].toLowerCase() !== s[j].toLowerCase()) {
+			return false
+		}
+	}
+	return true
 }
