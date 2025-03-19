cbc23f7fed9e0b00fecbf16c381ad2081d24e51b
dtn-prog <dtncode@gmail.com>
2025-03-19 21:26:49 +0700

    125 valid palindrome in python

--- python/125ValidPalindrome.py
+++ python/125ValidPalindrome.py
@@ -1,3 +1,21 @@
+from icecream import ic
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        pass
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if not s[i].isalnum():
+                i+=1
+                continue
+            if not s[j].isalnum():
+                j-=1
+                continue
+            
+            if s[i].lower() != s[j].lower():
+                return False
+            i+=1
+            j-=1
+        return True
+        
+so = Solution()
+ic(so.isPalindrome('tab a cat'))
